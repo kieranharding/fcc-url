6a8f3cba86a607082374d8de56b558d8fde79afb
Kieran Harding <kieranharding@gmail.com>
2017-01-08 10:44:38 -0600

    Fix Issue 1
    The api will now always return a JSON object (except for landing page),
    whether it is a good result or an error.

--- index.js
+++ index.js
@@ -1,16 +1,15 @@
 const express = require('express')
-// const http = require('http')
 const path = require('path')
 const base58 = require('base58')
 const pg = require('pg')
 const url = require('url')
 
-// pg.defaults.ssl = true
-
 const app = express()
 
-const params = url.parse(process.env.DATABASE_URL);
-const auth = params.auth.split(':');
+/* poolConfig is taken from the node-postgres documentation, since it doesn't
+    read DATABASE_URL directly. */
+const params = url.parse(process.env.DATABASE_URL)
+const auth = params.auth.split(':')
 
 const poolConfig = {
   user: auth[0],
@@ -26,18 +25,18 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/index.html'))
 })
 
-app.get('/testdb', (req, response) => {
-  pool.query('SELECT * FROM list', (err, result) => {
-    if (err) response.end(err)
-    else response.end(result.rows[0].id + ': ' + result.rows[0].url)
-  })
-})
+// app.get('/testdb', (req, response) => {
+//   pool.query('SELECT * FROM list', (err, result) => {
+//     if (err) response.end(err)
+//     else response.end(result.rows[0].id + ': ' + result.rows[0].url)
+//   })
+// })
 
-app.get('/testpr', (req, response) => {
-  pool.query('SELECT * FROM list')
-    .then((result) => response.end(result.rows[0].id + ' ' + result.rows[0].url))
-    .catch((err) => response.end(err.message))
-})
+// app.get('/testpr', (req, response) => {
+//   pool.query('SELECT * FROM list')
+//     .then((result) => response.end(result.rows[0].id + ' ' + result.rows[0].url))
+//     .catch((err) => response.end(err.message))
+// })
 
 app.get('/new/*', (request, response) => {
   function send_result (response, original, short) { // Do I need to pass in the response? Probably not.
@@ -57,8 +56,8 @@ app.get('/new/*', (request, response) => {
     pool.query('SELECT id FROM list WHERE url = $1', [arg_url], (err, result) => {
       if (err) {
         console.log(err.message, err.stack)
-        response.writeHead(500, {'content-type': 'text/plain'})
-        response.end('An error occured.')
+        response.writeHead(500, {'content-type': 'text/json'})
+        response.end(JSON.stringify({'Error': "An error occured fetching this item from the database."}))
       }
 
       // If it doesn't exist, add the url to the database
@@ -66,8 +65,8 @@ app.get('/new/*', (request, response) => {
         pool.query('INSERT INTO list (url) VALUES ($1) RETURNING id', [arg_url], (err, result) => {
           if (err) {
             console.log(err.message, err.stack)
-            response.writeHead(500, {'content-type': 'text/plain'})
-            response.end('An error occured.')
+            response.writeHead(500, {'content-type': 'text/json'})
+            response.end(JSON.stringify({'Error': 'An error occured adding this item to the database.'}))
           }
 
           send_result(response, arg_url, base58.encode(result.rows[0].id))
@@ -79,16 +78,16 @@ app.get('/new/*', (request, response) => {
     
     // Return a JSON string with the full and short urls
   } else {
-    response.writeHead(500, {'content-type': 'text/plain'})
-    response.end(request.params[0] + ' is not a valid URL.')
+    response.writeHead(500, {'content-type': 'text/json'})
+    response.end(JSON.stringify({'Error': request.params[0] + ' is not a valid URL.'}))
   }
 })
 
 app.get('/:url_id', (request, response) => {
   // If the id is base58, decode it.
   if (!/^[1-9a-km-zA-HJK-NP-Z]*$/.test(request.params.url_id)) {
-    response.writeHead(500, {'content-type': 'text/plain'})
-    response.end(request.params.url_id + ' is not a valid code.')
+    response.writeHead(500, {'content-type': 'text/json'})
+    response.end(JSON.stringify({'Error': request.params.url_id + ' is not a valid code.'}))
   } else {
     // If the decoded id is in the database, get the url
     const db_id = base58.decode(request.params.url_id)
@@ -99,17 +98,18 @@ app.get('/:url_id', (request, response) => {
           response.redirect(result.rows[0].url)
         } else {
           // If the decoded id is not in the database, return an error message
-          response.writeHead(500, {'content-type': 'text/plain'})
-          response.end(request.params.url_id + ' is not in the database.')
+          response.writeHead(500, {'content-type': 'text/json'})
+          response.end(JSON.stringify({'Error': request.params.url_id + ' is not in the database.'}))
         }
       })
       .catch((err) => {
-        console.log(err.message)
-        response.writeHead(500, {'content-type': 'text/plain'})
-        response.end('An error occured')
+        console.log(err.message, err.stack)
+        response.writeHead(500, {'content-type': 'text/json'})
+        response.end(JSON.stringify({'Error': 'An error occured'}))
       })
   }
 })
+
 const port = process.env.PORT || 5000
 app.listen(port)
 console.log('Now listening on port ' + port) 
